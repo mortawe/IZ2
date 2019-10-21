6edd1f536cd7f043c73091501b2fc889936e451e
mortawe <mortawe@gmail.com>
2019-10-21 23:57:31 +0300

    modi

--- example/main.c
+++ example/main.c
@@ -79,3 +79,17 @@ void parallel(){
     free(a);
 }
 
+int main(){
+    clock_t begin = clock();
+    parallel();
+    clock_t end = clock();
+    double time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
+    printf("parallel : %f \n", time_spent);
+
+    begin = clock();
+    sequential();
+    end = clock();
+    time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
+    printf("sequential : %f \n ", time_spent);
+    return 0;
+}
--- test/unit_test.cpp
+++ test/unit_test.cpp
@@ -6,3 +6,21 @@
 extern "C" {
 #include "main.h"
 };
+
+TEST(test_seq, test_seq_test_fill_Test){
+    int * a = (int*)malloc(memory_size);
+    if (a == NULL){
+        exit(ERROR_MALLOC);
+    }
+    a = fill_array_seq(a, memory_size / sizeof(int));
+    int count = 0;
+    for (int i = 0; i < memory_size / sizeof(int); i++){
+        ASSERT_EQ(a[i], count);
+        count = (count + 1) % 4;
+    }
+}
+
+int main(int argc, char **argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
